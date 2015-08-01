f4a3b08ce9018e43b1e373c6b3412ac1fde566be
Rob Looby <robertjlooby@gmail.com>
2015-08-01 11:08:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# elm-bowling-game-kata
+
+The [bowling game kata](http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata) in Elm.
+
+## Tests
+
+`elm-test test/AllTests.elm`
---
+++ test/GameTest.elm
@@ -0,0 +1,9 @@
+module GameTest where
+
+import Game exposing (..)
+import ElmTest.Assertion exposing (assertEqual, Assertion)
+import ElmTest.Test exposing (test, suite)
+
+tests =
+  suite "Game"
+    []
